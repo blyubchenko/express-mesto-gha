2d52cb56b8f470e94983f5a941a8afb6e32f137f
Boris Lyubchenko <Blyubchenko@ya.ru>
2023-07-03 18:51:01 +0800

    fix: add populate in postCard

--- controllers/cards.js
+++ controllers/cards.js
@@ -38,6 +38,7 @@ const postCard = (req, res, next) => {
     ...req.body,
     owner: req.user._id,
   })
+    .populate('owner')
     .then((card) => res.status(HTTP_CREATED).send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
